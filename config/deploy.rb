db7a2ab9b71319d2b0d94442fc9071d499291f39
Eduardo Gonzalez <edugonch@gmail.com>
2018-07-14 00:31:08 -0600

    Fix passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :passenger_environment_variables, { :path => '/home/deploy/.rvm/gems/ruby-2.5.1/gems/passenger-5.3.3/bin:$PATH' }
 set :passenger_restart_command, '/home/deploy/.rvm/gems/ruby-2.5.1/gems/passenger-5.3.3/bin/passenger-config restart-app'
 
-
+set :passenger_in_gemfile, true
+set :passenger_restart_with_sudo, true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
