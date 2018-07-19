9dce962bf75f012f1be3b5e094590d4e7246646f
hdiguense <ldiego008@gmail.com>
2018-07-18 23:04:31 -0600

    Reference page

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -1,2 +1,4 @@
 class ReferencesController < ApplicationController
+	def	index
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
 
   get "corredor_seco", to: "ca_dry_corridor#index", as: :ca_dry_corridor
   get "corredor_seco/get_images", to: "ca_dry_corridor#get_images", as: :ca_dry_corridor_get_images
+  
+  get "estado_de_la_vegetacion", to: "state_of_vegetation#index", as: :state_of_vegetation
+  get "estado_de_la_vegetacion/get_images", to: "state_of_vegetation#get_images", as: :state_of_vegetation_get_images
+  
+  get "referencias", to: "references#index", as: :references
 end
